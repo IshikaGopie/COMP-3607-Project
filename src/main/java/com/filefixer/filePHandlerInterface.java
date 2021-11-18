34229b8b9ef5c20113503a50d82a78a4b1c488f4
alphonso-a <alphonsobalbosa@gmail.com>
2021-11-18 10:28:44 -0400

    commit

--- src/main/java/com/filefixer/filePHandlerInterface.java
+++ src/main/java/com/filefixer/filePHandlerInterface.java
@@ -2,6 +2,6 @@ package com.filefixer;
 
 import java.io.File;
 
-public interface filePathHandlerInterface{
+public interface filePHandlerInterface{
     public String create_PATH(File pdf, student student);
 }
