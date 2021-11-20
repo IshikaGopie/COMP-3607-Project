faeeac31e145bc0d4a4428d0572ec8fa3ddca5e8
Ishika_Gopie <60522247+IshikaGopie@users.noreply.github.com>
2021-11-20 12:11:12 -0400

    Update directoryHandler.java

--- src/main/java/com/filefixer/directoryHandler.java
+++ src/main/java/com/filefixer/directoryHandler.java
@@ -13,4 +13,14 @@ public class directoryHandler implements directoryHandlerInterface{
             System.out.println("ERROR: " + e.getMessage());
         }
     }
+    
+    public void copyDirectory(String sourceDirectory, String destinationDirectory){
+        File sourceFile = new File(sourceDirectory); 
+        File destFile = new File(destinationDirectory);
+        try {
+            FileUtils.copyDirectory(sourceFile, destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
