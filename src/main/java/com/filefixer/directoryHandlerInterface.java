a209e4a4016537825a38ec3edcbe79e9ad3d0a2f
Ishika_Gopie <60522247+IshikaGopie@users.noreply.github.com>
2021-11-20 12:10:34 -0400

    Update directoryHandlerInterface.java

--- src/main/java/com/filefixer/directoryHandlerInterface.java
+++ src/main/java/com/filefixer/directoryHandlerInterface.java
@@ -2,4 +2,5 @@ package com.filefixer;
 
 public interface directoryHandlerInterface{
     public void newDirectory(String path);
+    public void copyDirectory(String sourceDirectory, String destinationDirectory);
 }
