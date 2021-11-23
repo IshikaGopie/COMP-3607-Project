45d2bc505f89b7b17d3c08e0ee2dc29d52d5b018
Ishika_Gopie <60522247+IshikaGopie@users.noreply.github.com>
2021-11-23 14:02:10 -0400

    Add files via upload

---
+++ src/test/java/com/filefixer/getPDFsTest.java
@@ -0,0 +1,38 @@
+package com.filefixer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+public class getPDFsTest{
+    @Test
+    void getPDFs(){
+        fileGetterInterface fileGetter = new fileGetter(new pdfCollection());
+        String[] name1 = new String[3];
+        String[] name2 = new String[3];
+        Collection<File> pdfFiles1 = new ArrayList<File>();;
+        Collection<File> pdfFiles2;
+        File file = new File("Anna Horton_601727_assignsubmission_file_info 2603 assignment 1.pdf");
+        pdfFiles1.add(file);
+        file = new File("Beth Morales-Horton_601683_assignsubmission_file_Assignment1_81305512.pdf");
+        pdfFiles1.add(file);
+        file = new File("Clifton Bowen_601680_assignsubmission_file_81380485.pdf");
+        pdfFiles1.add(file);
+        pdfFiles2 = fileGetter.getFiles("testFiles/testingFolder");
+        int i = 0;
+        for(File files: pdfFiles1){
+            name1[i] = files.getName();
+            i += 1;
+        }
+         i = 0;
+        for(File files: pdfFiles2){
+            name2[i] = files.getName();
+            i += 1;
+        }
+        assertEquals(name1[0], name2[0]);
+        assertEquals(name1[1], name2[1]);
+        assertEquals(name1[2], name2[2]);
+    }
+}
