d55cf0b5e197c93de1c7a0472c25d9146fc49fbd
Ishika_Gopie <60522247+IshikaGopie@users.noreply.github.com>
2021-11-23 13:45:28 -0400

    Update pdfPathHandlerInterface.java

--- src/main/java/com/filefixer/pdfPathHandlerInterface.java
+++ src/main/java/com/filefixer/pdfPathHandlerInterface.java
@@ -11,5 +11,5 @@ public interface pdfPathHandlerInterface{
 	 * @param student The current student instance.
 	 * @return String: The destination file-path of the current PDF.
 	 */
-    public String createPath(File pdf, student student);
+    public String createPath(File pdf, student student, String directory);
 }
