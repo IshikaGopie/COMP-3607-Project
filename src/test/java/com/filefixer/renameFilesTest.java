4e1a7d9c91ef9c4543912b3cf74c7dcd6a7bf425
Ishika_Gopie <60522247+IshikaGopie@users.noreply.github.com>
2021-11-23 14:02:33 -0400

    Add files via upload

---
+++ src/test/java/com/filefixer/renameFilesTest.java
@@ -0,0 +1,41 @@
+package com.filefixer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.util.Collection;
+public class renameFilesTest {
+    private String[] name1 = new String[3];
+    private String[] name2 = new String[3];
+    @Test
+    void renameFilesTest(){
+        name1[0] = "Anna Horton_601727_assignsubmission_file_Anna Horton_601727_assignsubmission_file_info 2603 assignment 1.pdf";
+        name1[1] = "Beth Morales-Horton_601683_assignsubmission_file_Beth Morales-Horton_601683_assignsubmission_file_Assignment1_81305512.pdf";
+        name1[2] = "Clifton Bowen_601680_assignsubmission_file_Clifton Bowen_601680_assignsubmission_file_81380485.pdf";
+        
+        fixingProcedureFacadeInterface fixingProcedure = new fixingProcedureFacade("testFiles/testFilesToRename/renamedFiles");
+
+
+        Collection<File> pdfFiles;
+        fileGetterInterface fileGetter = new fileGetter(new pdfCollection());
+        pdfFiles = fileGetter.getFiles("testFiles/testFilesToRename");
+
+
+        Collection<File> csvFiles;
+        fileGetter.changeFileCollectionStrategy(new csvCollection());
+        csvFiles = fileGetter.getFiles("testFiles/testFilesToRename");
+
+        fixingProcedure.renameAndMovePdfs(pdfFiles, csvFiles);
+        File pdfFiles2 = new File("testFiles/testFilesToRename/renamedFiles/");
+        File[] allFiles = pdfFiles2.listFiles();
+        int i = 0;
+        for(File files: allFiles){
+            name2[i] = files.getName();
+            i += 1;
+        }
+        assertEquals(name1[0], name2[0]);
+        assertEquals(name1[1], name2[1]);
+        assertEquals(name1[2], name2[2]);
+    }
+}
