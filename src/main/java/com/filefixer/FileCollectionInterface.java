f72bb2a1473862e27d3b0856908504ec9c63e7cf
alphonso-a <62738757+alphonso-a@users.noreply.github.com>
2021-11-23 18:08:46 -0400

    Update FileCollectionInterface.java

--- src/main/java/com/filefixer/FileCollectionInterface.java
+++ src/main/java/com/filefixer/FileCollectionInterface.java
@@ -7,7 +7,7 @@ import java.util.Collection;
  * Used as the Strategy Design Pattern's interface for the following sub-types, {@link csvCollection}, 
  * {@link pdfCollection} and {@link zipCollection}.
  */
-public interface fileCollectionInterface{
+public interface FileCollectionInterface{
     /**
      * @param path The folder-path of the directory.
      * @return {@link Collection}<{@link File}>: A collection of all files.
